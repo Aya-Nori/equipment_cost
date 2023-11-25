9681d698e5c748412957e7d62da69aad7f6cec3c
Aya-Nori <atc.btm@gmail.com>
2023-11-25 21:54:17 +0900

    マイグレーションファイル編集

--- db/migrate/20231118121615_create_equipment.rb
+++ db/migrate/20231118121615_create_equipment.rb
@@ -2,9 +2,8 @@ class CreateEquipment < ActiveRecord::Migration[7.0]
   def change
     create_table :equipment do |t|
       t.string :equipment_name, null: false
-      t.date :start_time
-      t.date :finish_time
       t.string :category, null: false
+      t.integer :price, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,8 @@
 ActiveRecord::Schema[7.0].define(version: 2023_11_18_121615) do
   create_table "equipment", charset: "utf8", force: :cascade do |t|
     t.string "equipment_name", null: false
-    t.date "start_time"
-    t.date "finish_time"
     t.string "category", null: false
+    t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
