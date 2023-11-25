20a8a3fb76eed141f355a653b9e78a94765dbf18
Aya-Nori <atc.btm@gmail.com>
2023-11-25 22:55:44 +0900

    equipment_index修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'analysisgroups/index'
   root to: 'analysisgroups#index'
-  resources :equipments, only: :index
+  resources :equipments, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
