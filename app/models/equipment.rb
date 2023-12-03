a0801576d65f6a4b0e7b6b7a5b32787f84c26c15
Aya-Nori <atc.btm@gmail.com>
2023-12-03 14:03:56 +0900

    price を数字に設定

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,5 +1,5 @@
 class Equipment < ApplicationRecord
   validates :equipment_name, presence: true
   validates :category, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: true
 end
