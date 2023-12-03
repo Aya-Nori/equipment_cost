196a0dbd5d8dbc8b06387ac400a01fbcc8715878
Aya-Nori <atc.btm@gmail.com>
2023-12-03 19:00:29 +0900

    開始終了時間の実装

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -16,6 +16,41 @@ class EquipmentsController < ApplicationController
     redirect_to '/'
   end
 
+  def start_analysis
+    @equipment = Equipment.find(params[:id])
+    start_finish_time = @equipment.start_finish_times.build(
+      start_time: Time.current,
+      condition: true,
+      user: current_user
+    )
+    
+    if start_finish_time.save
+      flash[:success] = '分析を開始しました。'
+    else
+      flash[:error] = '分析の開始に失敗しました。'
+    end
+  
+    redirect_to @equipment
+  end
+  
+  def finish_analysis
+    @equipment = Equipment.find(params[:id])
+    start_finish_time = @equipment.start_finish_times.build(
+      finish_time: Time.current,
+      condition: false,
+      user: current_user
+    )
+    
+    if start_finish_time.save
+      flash[:success] = '分析を終了しました。'
+    else
+      flash[:error] = '分析の終了に失敗しました。'
+    end
+  
+    redirect_to @equipment
+  end
+
+
   private
   def equipment_params
     params.require(:equipment).permit(:equipment_name, :category, :price)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'analysisgroups#index'
-  resources :equipments, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :equipments, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    member do
+      match 'start_analysis', via: [:get, :post]
+      match 'finish_analysis', via: [:get, :post]
+    end
+  end
 end
+
