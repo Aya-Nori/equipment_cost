538d65c6d803aa6d7369d2f0eabcaf595337a134
Aya-Nori <atc.btm@gmail.com>
2023-12-03 13:27:39 +0900

    equipmentコントローラー修正

--- app/controllers/equipments_controller.rb
+++ app/controllers/equipments_controller.rb
@@ -11,9 +11,18 @@ class EquipmentsController < ApplicationController
     @equipment = Equipment.find(params[:id])
   end
 
+  # def create
+  #   Equipment.create(equipment_params)
+  #   redirect_to '/'
+  # end
   def create
-    Equipment.create(equipment_params)
-    redirect_to '/'
+    @equipment = Equipment.new(equipment_params)
+    if @equipment.save
+      redirect_to '/'
+    else
+      Rails.logger.error(@equipment.errors.full_messages.to_sentence)
+      render :new
+    end
   end
 
   private
