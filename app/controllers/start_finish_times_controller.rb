fd0b3e06d0bd6185fa634579b4cc6e72fb5b2f3f
Aya-Nori <atc.btm@gmail.com>
2024-01-15 16:41:12 +0900

    部署ごとのリスト表示ボタン実装

--- app/controllers/start_finish_times_controller.rb
+++ app/controllers/start_finish_times_controller.rb
@@ -1,5 +1,15 @@
 class StartFinishTimesController < ApplicationController
+
   def index
-    @start_finish_times = StartFinishTime.includes(:user, :equipment).all
+    @divisions = User.pluck(:division).uniq
+    selected_division = params[:selected_division]
+
+    if selected_division.present?
+      @start_finish_times = StartFinishTime.joins(:user).where(users: { division: selected_division })
+      @selected_division = params[:selected_division]
+    else
+      @start_finish_times = StartFinishTime.all
+    end
   end
+
 end
