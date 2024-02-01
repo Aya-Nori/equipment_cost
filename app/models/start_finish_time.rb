02fc2ed4abd0570b573f7ce422d9fb43d4748123
Aya-Nori <atc.btm@gmail.com>
2024-02-01 11:28:29 +0900

    グラフ実装中

--- app/models/start_finish_time.rb
+++ app/models/start_finish_time.rb
@@ -6,4 +6,11 @@ class StartFinishTime < ApplicationRecord
   belongs_to :user
   belongs_to :equipment
 
+  def total_calc_price
+    # ここに計算ロジックを追加する
+    # 例: 仮に利用料金が「usage_fee」というカラムに保存されている場合
+    usage_fee
+  end
+
+
 end
